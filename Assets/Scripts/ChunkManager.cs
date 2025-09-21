417bc5382b013d467a7734ea370d9237fbeefc7f
MYB-06 <06myb06@gmail.com>
2025-09-21 04:27:29 +0300

    Refactor: Rename SpawnManager to ChunkManager

--- Assets/Scripts/ChunkManager.cs
+++ Assets/Scripts/ChunkManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
+public class ChunkManager : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private ObjectPool chunkPool;
